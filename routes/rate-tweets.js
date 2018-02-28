808c00b05c261c3757e2723977f0f13339a91894
SDob1993 <Simon.Dobiosz@stud.uni-regensburg.de>
2018-02-28 17:33:34 +0100

    versuch über cookie

--- routes/rate-tweets.js
+++ routes/rate-tweets.js
@@ -7,19 +7,13 @@ var uid;
 var url = 'mongodb://infophil:ethtweets8897@ethicaltweets-shard-00-00-nzg80.mongodb.net:27017,ethicaltweets-shard-00-01-nzg80.mongodb.net:27017,ethicaltweets-shard-00-02-nzg80.mongodb.net:27017/test?ssl=true&replicaSet=ethicaltweets-shard-0&authSource=admin';
 var currtweet={};
 var item={};
-console.log("leeres obj");
+
 
 var start ="START";
 
 router.get('/', function(req, res, next) {
   uid = req.cookies.uid;
 
-  res.render('rate-tweets', { title: 'Ethical Tweets', contenthtml:currtweet.content, namehtml:currtweet.handle,
-      datehtml:currtweet.date,starthtml:start});
-
-});
-
-router.get('/get-tweet', function(req, res, next) {
     mongo.connect(url, function(err, client) {
         assert.equal(null, err);
         start="";
@@ -28,36 +22,52 @@ router.get('/get-tweet', function(req, res, next) {
         col.aggregate([{$sample: {size: 1}}])
             .each(function(err, doc) {
                 if(doc) {
-                  currtweet=doc;
+                    currtweet=doc;
                     console.log("tweet aus db geholt und ueberschrieben");
                     overwriteItem();
                 }
-                    client.close();
-                    return false;
-                });
+                client.close();
+                return false;
+            });
     });
     function overwriteItem(){
         console.log(currtweet);
 
-        item = {
-            word: currtweet.word,
-            suppcategory0: currtweet.category0,
-            suppcategory1: currtweet.category1,
-            suppcategory2: currtweet.category2,
-            ratedcategory: req.body.kategorie,
-            userid: uid
-        };
-        console.log("item überschrieben");
+        res.cookie('wordc',currtweet.word);
+        res.cookie('suppcategory0',currtweet.category0);
+        res.cookie('suppcategory1',currtweet.category1);
+        res.cookie('suppcategory2',currtweet.category2);
+        res.cookie('ratedcategory',currtweet.ratedcategory);
 
-        res.redirect('/rate-tweets');
+
+
+        console.log("values in cookies");
+
+        res.render('rate-tweets', { title: 'Ethical Tweets', contenthtml:currtweet.content, namehtml:currtweet.handle,
+            datehtml:currtweet.date,starthtml:start});
     }
+
+
+
+});
+
+router.get('/get-tweet', function(req, res, next) {
+
 
 });
 
 router.post('/insert', function(req, res, next){
 
-    item.ratedcategory=parseInt(req.body.kategorie);
-    console.log("kat überschrieben");
+    item = {
+        word: req.cookies.wordc,
+        suppcategory0: parseInt(req.cookies.suppcategory0),
+        suppcategory1: parseInt(req.cookies.suppcategory1),
+        suppcategory2: parseInt(req.cookies.suppcategory2),
+        ratedcategory: parseInt(req.body.kategorie),
+        userid: uid
+    };
+
+    console.log("item mit werten aus cookie");
     mongo.connect(url, function (err, client) {
         assert.equal(null, err);
         var db = client.db('users');
@@ -67,7 +77,7 @@ router.post('/insert', function(req, res, next){
             client.close();
         });
     });
-    res.redirect('/rate-tweets/get-tweet');
+    res.redirect('/rate-tweets');
     console.log("get tweet");
 });
 
