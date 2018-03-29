119fbbd7294c229dcdaf1e3aaa8e82cff61b2700
SDob1993 <Simon.Dobiosz@stud.uni-regensburg.de>
2018-03-29 15:08:41 +0200

    comments added

--- routes/rate-tweets.js
+++ routes/rate-tweets.js
@@ -15,18 +15,21 @@ var start ="START";
 router.get('/', function(req, res, next) {
 
 
-
+  //setting id from cookie
   uid = req.cookies.uid;
 
-
+    //connecting
     mongo.connect(url, function(err, client) {
         assert.equal(null, err);
+        //setting empty content (text-field)
         start="";
         var db = client.db('users');
-        var col = db.collection('tweets');
+        var col = db.collection('tweets
+        //get random tweet from db
         col.aggregate([{$sample: {size: 1}}])
             .each(function(err, doc) {
                 if(doc) {
+                    //setting currtweet to object
                     currtweet=doc;
                     console.log("tweet aus db geholt und ueberschrieben");
                     overwriteItem();
@@ -36,7 +39,7 @@ router.get('/', function(req, res, next) {
             });
     });
     function overwriteItem(){
-        console.log(currtweet);
+        //set information from tweet needed as cookies
 
         res.cookie('wordc',currtweet.word);
         res.cookie('suppcategory0',currtweet.category0);
@@ -46,7 +49,7 @@ router.get('/', function(req, res, next) {
 
 
 
-        console.log("values in cookies");
+        //render new textfield
 
         res.render('rate-tweets', { title: 'Ethical Tweets', contenthtml:currtweet.content, namehtml:currtweet.handle,
             datehtml:currtweet.date,starthtml:start});
@@ -65,8 +68,12 @@ router.post('/insert', function(req, res, next){
 
 
     uid = req.cookies.uid;
+  //counter to 10
     var locount = parseInt(req.cookies.counter+1);
     res.cookie("counter",locount);
+   //creating new object with user data input
+    //storing user input data in cookies solves the problem that more than
+    //one user is currently rating the tweets(app runs only one instance)
 
 
     item = {
@@ -82,16 +89,19 @@ router.post('/insert', function(req, res, next){
     mongo.connect(url, function (err, client) {
         assert.equal(null, err);
         var db = client.db('users');
+      //callback and inserting
         db.collection('result').insertOne(item, function (err, result) {
             assert.equal(null, err);
             console.log('result data inserted');
             client.close();
         });
     });
+  //if 10 tweets are rated redirecting to keywords
     if(req.cookies.counter=="111111111"){
         res.cookie("counter",0);
         res.redirect('/rate-keywords');
     }
+  //else self-redirect
     res.redirect('/rate-tweets');
     console.log("get tweet");
 });
